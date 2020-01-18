0ed31314d659a42059904d4e5bef3753213afb30
石田航也 <aichibaseball@ishidawatarunari-no-MacBook-Air.local>
2020-01-18 16:24:06 +0900

    learn jsx syntax
    jsxについて学びました

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello World</h1></div>;
+    return (
+      <label htmlFor="bar">bar</label>
+      <input type="text" onClick={()=> console.log("i`m clicked")} />
+    );
   }
 }
 
