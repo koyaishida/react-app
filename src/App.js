84c854435d7414b7b0f0b17a24ae060de38c15de
石田航也 <aichibaseball@ishidawatarunari-no-MacBook-Air.local>
2020-01-18 16:27:30 +0900

    div タグの追加
    前回のエラーの修正

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={()=> console.log("i`m clicked")} />
+      <div>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onClick={()=> console.log("i`m clicked")} />
+      </div>
     );
   }
 }
