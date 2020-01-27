10bf2a31dfb734c3e27f47988f7136393cab23e7
石田航也 <aichibaseball@ishidawatarunari-no-MacBook-Air.local>
2020-01-27 19:44:36 +0900

    material-ui2
    new-evnet,show-eventのデザイン

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {Field,reduxForm} from "redux-form";
 import {Link} from "react-router-dom";
+import RaisedButton from "material-ui/RaisedButton"
+import TextField from "material-ui/TextField"
 
 import {getEvent,deleteEvent,putEvent} from "../reducers/actions";
 
@@ -19,10 +21,14 @@ class EventsShow extends Component {
   renderField(field){
     const {input, label, type, meta: {touched, error}} = field
     return (
-    <div>
-      <input {...input} placeholder={label} type={type}/>
-      {touched && error &&<span>{error}</span>}
-    </div>
+      <TextField 
+      hintText={label}
+      floatingLabelText={label}
+      type={type}
+      errorText={touched && error}
+      {...input}
+      fullWidth={true}
+      />
     )
   }
 
@@ -39,6 +45,8 @@ class EventsShow extends Component {
 
   render(){
     const {handleSubmit,pristine,submitting,invalid } = this.props
+    const style = {margin :12}
+
     return(
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
@@ -46,9 +54,11 @@ class EventsShow extends Component {
           <Field label="Body" name="body" type="text" component={this.renderField} />
         </div>
         <div>
-          <input type="submit" value="submit" disabled={pristine|| submitting || invalid} />
-          <Link to="/">Cancel</Link>
-          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
+          <RaisedButton label="Submit" type="submit" style={style} disabled={pristine|| submitting || invalid} />
+
+          <RaisedButton label="Cancel" style={style} containerElement={<Link to="/">Cancel</Link>}/>
+
+          <RaisedButton label="Delete" style={style} containerElement={<Link to="/" />} onClick={this.onDeleteClick}/>
         </div>
       </form>
     )
